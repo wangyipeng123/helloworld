baef92784e2e056499bc5cc62e10ae367d4ac557
Administrator <Administrator@4XP02ONO0OCE2UH>
2017-09-09 12:32:55 +0800

    代碼整理

--- src/main/java/com/wyh/Test.java
+++ src/main/java/com/wyh/Test.java
@@ -1,8 +1,8 @@
 package com.wyh;
 
 public class Test {
- public static void main(String[] args) {
-	System.out.println("hello world");
-}
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
 
 }
